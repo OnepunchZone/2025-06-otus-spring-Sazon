78a75ea3a1a5e179c527ce2fb114f0a9ba345186
Alexandr <onepunch.zone@yandex.ru>
2025-08-05 03:07:49 +0500

    hw04

---
+++ hw04-spring-boot-shell/src/test/java/ru/otus/hw/CsvQuestionDaoTest.java
@@ -0,0 +1,57 @@
+package ru.otus.hw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@DisplayName("Тест CsvQuestionDao на передачу существующего и не существующего ресурса")
+public class CsvQuestionDaoTest {
+    @Mock
+    private TestFileNameProvider testFileNameProvider;
+    private CsvQuestionDao csvQuestionDao;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        csvQuestionDao = new CsvQuestionDao(testFileNameProvider);
+    }
+
+    @Test
+    @DisplayName("Загрузка вопросов из существующего файла")
+    void findAll_ShouldReturnQuestions_IfFileExists() {
+        when(testFileNameProvider.getTestFileName()).thenReturn("questions.csv");
+
+        List<Question> questions = csvQuestionDao.findAll();
+
+        assertNotNull(questions);
+        assertEquals(5, questions.size(), "Must be 5 questions");
+
+        Question firstQuestion = questions.get(0);
+        assertEquals("Is there life on Mars?", firstQuestion.text());
+        assertEquals(3, firstQuestion.answers().size(), "Must be 3 answers");
+    }
+
+    @Test
+    @DisplayName("Бросает исключение при отсутствии файла")
+    void findAll_ShouldThrowException_IfFileNotExists() {
+        when(testFileNameProvider.getTestFileName()).thenReturn("non-existent-file.csv");
+
+        Exception exception = assertThrows(QuestionReadException.class, () -> {
+            csvQuestionDao.findAll();
+        });
+
+        assertTrue(exception.getMessage().contains("File not found"));
+    }
+
+}
